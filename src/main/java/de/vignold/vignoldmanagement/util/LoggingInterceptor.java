fd2ec069aa78b54d17bd043845643cd341ea8f8e
b.sedef <b.sedef@vignold.de>
2022-06-19 20:07:07 +0300

    login interceptor added

---
+++ src/main/java/de/vignold/vignoldmanagement/util/LoggingInterceptor.java
@@ -0,0 +1,34 @@
+package de.vignold.vignoldmanagement.util;
+
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.springframework.web.servlet.AsyncHandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoggingInterceptor implements AsyncHandlerInterceptor
+{
+    private static final Log LOG = LogFactory.getLog(LoggingInterceptor.class);
+
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+    {
+        StringBuilder logMessage = new StringBuilder();
+        logMessage.append("method: ").append(request.getMethod()).append("\t");
+        logMessage.append("uri: ").append(request.getRequestURI()).append("\t");
+        logMessage.append("status: ").append(response.getStatus()).append("\t");
+        logMessage.append("remoteAddress: ").append(request.getRemoteAddr()).append("\t");
+
+        if (ex != null)
+        {
+            LoggingInterceptor.LOG.error(logMessage.toString(), ex);
+        }
+        else
+        {
+            LoggingInterceptor.LOG.info(logMessage.toString());
+        }
+    }
+
+}
